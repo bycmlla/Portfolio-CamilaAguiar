7938e5005a4e1d73c5c1a351be78ff69e342304b
Camila de Carvalho <by.cmlla0107@gmail.com>
2024-08-10 11:46:38 -0300

    fix

--- src/pages/Projects/SubPages/SQLPage.js
+++ src/pages/Projects/SubPages/SQLPage.js
@@ -1,4 +1,4 @@
-import SQLImage from "../../../assets/images/sql.png";
+import SQLImage from "../../../assets/images/sql-illustration.png";
 import { useRef, useState } from "react";
 import NavBar from "../../../components/NavBar/NavBar";
 import "./SQLPage.css";
@@ -27,163 +27,217 @@ const SQLPage = () => {
   return (
     <div className={`SQLPage-container ${isDarkMode ? "dark-mode-sql" : ""}`}>
       <NavBar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
-      <img src={SQLImage} alt="" width="30%" height="50%" />
+      <img
+        className="image-sql"
+        src={SQLImage}
+        alt=""
+        width="100%"
+        height="20%"
+      />
       <div className="SQLPage">
-        <h1>SQL</h1>
-
-        <nav>
-          <h2>Índice</h2>
-          <ul>
+        <p>
+          SQL (Structured Query Language) é uma linguagem padrão utilizada para
+          gerenciar e manipular bancos de dados relacionais. Ela permite que
+          você crie, leia, atualize e exclua dados armazenados em tabelas dentro
+          de um banco de dados.
+          <br />
+          <br /> Este é um tutorial com informações sobre algumas
+          funcionalidades do SQl e como utilizá-los. Abaixo temos o sumário com
+          todos assuntos abordados aqui até então tornando sua navegação mais
+          fácil. Logo adicionarei mais conteúdos!
+        </p>
+        <nav className="custom-nav">
+          <h2 className="nav-title">Sumário</h2>
+          <ul className="nav-list">
             <li>
-              <a href="#paginas-extras">Páginas Extras</a>
-            </li>
-            <li>
-              <a href="#mysql-sql-server">
+              <a className="nav-link" href="#mysql-sql-server">
                 MySQL ou SQL Server: Qual Utilizar?
               </a>
-              <ul>
+              <ul className="sub-list">
                 <li>
-                  <a href="#licenciamento-custo">Licenciamento e Custo</a>
+                  <a className="sub-link" href="#licenciamento-custo">
+                    Licenciamento e Custo
+                  </a>
                 </li>
                 <li>
-                  <a href="#linguagem-sintaxe-sql">Linguagem e Sintaxe SQL</a>
+                  <a className="sub-link" href="#linguagem-sintaxe-sql">
+                    Linguagem e Sintaxe SQL
+                  </a>
                 </li>
                 <li>
-                  <a href="#suporte-tipos-dados">Suporte a Tipos de Dados</a>
+                  <a className="sub-link" href="#suporte-tipos-dados">
+                    Suporte a Tipos de Dados
+                  </a>
                 </li>
                 <li>
-                  <a href="#procedimentos-armazenados-funcoes">
+                  <a
+                    className="sub-link"
+                    href="#procedimentos-armazenados-funcoes"
+                  >
                     Procedimentos Armazenados e Funções
                   </a>
                 </li>
                 <li>
-                  <a href="#ferramentas-administracao">
+                  <a className="sub-link" href="#ferramentas-administracao">
                     Ferramentas de Administração
                   </a>
                 </li>
                 <li>
-                  <a href="#seguranca">Segurança</a>
+                  <a className="sub-link" href="#seguranca">
+                    Segurança
+                  </a>
                 </li>
                 <li>
-                  <a href="#integracao-outras-tecnologias">
+                  <a className="sub-link" href="#integracao-outras-tecnologias">
                     Integração com Outras Tecnologias
                   </a>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#indices-sql">Índices no SQL</a>
-              <ul>
+              <a className="nav-link" href="#indices-sql">
+                Índices no SQL
+              </a>
+              <ul className="sub-list">
                 <li>
-                  <a href="#o-que-sao-indices">O que são índices?</a>
+                  <a className="sub-link" href="#o-que-sao-indices">
+                    O que são índices?
+                  </a>
                 </li>
                 <li>
-                  <a href="#indice-clusterizado">Índice Clusterizado</a>
+                  <a className="sub-link" href="#indice-clusterizado">
+                    Índice Clusterizado
+                  </a>
                 </li>
                 <li>
-                  <a href="#indice-nao-clusterizado">Índice Não Clusterizado</a>
+                  <a className="sub-link" href="#indice-nao-clusterizado">
+                    Índice Não Clusterizado
+                  </a>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#views">Views</a>
-              <ul>
+              <a className="nav-link" href="#views">
+                Views
+              </a>
+              <ul className="sub-list">
                 <li>
-                  <a href="#o-que-e-view">O que é uma view (exibição)?</a>
+                  <a className="sub-link" href="#o-que-e-view">
+                    O que é uma view (exibição)?
+                  </a>
                 </li>
                 <li>
-                  <a href="#tipos-views">Tipos de Views</a>
-                  <ul>
+                  <a className="sub-link" href="#tipos-views">
+                    Tipos de Views
+                  </a>
+                  <ul className="sub-sub-list">
                     <li>
-                      <a href="#views-simples">Views Simples</a>
+                      <a className="sub-sub-link" href="#views-simples">
+                        Views Simples
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-complexas">Views Complexas</a>
+                      <a className="sub-sub-link" href="#views-complexas">
+                        Views Complexas
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-indexadas">Views Indexadas</a>
+                      <a className="sub-sub-link" href="#views-indexadas">
+                        Views Indexadas
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-particao">Views de Partição</a>
+                      <a className="sub-sub-link" href="#views-particao">
+                        Views de Partição
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-sistema">Views do Sistema</a>
+                      <a className="sub-sub-link" href="#views-sistema">
+                        Views do Sistema
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-atualizacao">Views de Atualização</a>
+                      <a className="sub-sub-link" href="#views-atualizacao">
+                        Views de Atualização
+                      </a>
                     </li>
                     <li>
-                      <a href="#views-encapsuladas">Views Encapsuladas</a>
+                      <a className="sub-sub-link" href="#views-encapsuladas">
+                        Views Encapsuladas
+                      </a>
                     </li>
                   </ul>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#joins">Joins</a>
-              <ul>
+              <a className="nav-link" href="#joins">
+                Joins
+              </a>
+              <ul className="sub-list">
                 <li>
-                  <a href="#o-que-sao-joins">O que são os Joins?</a>
+                  <a className="sub-link" href="#o-que-sao-joins">
+                    O que são os Joins?
+                  </a>
                 </li>
                 <li>
-                  <a href="#tipos-joins">Tipos de Joins</a>
-                  <ul>
+                  <a className="sub-link" href="#tipos-joins">
+                    Tipos de Joins
+                  </a>
+                  <ul className="sub-sub-list">
                     <li>
-                      <a href="#inner-join">Inner Join</a>
+                      <a className="sub-sub-link" href="#inner-join">
+                        Inner Join
+                      </a>
                     </li>
                     <li>
-                      <a href="#left-join">Left Join (Left Outer Join)</a>
+                      <a className="sub-sub-link" href="#left-join">
+                        Left Join (Left Outer Join)
+                      </a>
                     </li>
                     <li>
-                      <a href="#right-join">Right Join (Right Outer Join)</a>
+                      <a className="sub-sub-link" href="#right-join">
+                        Right Join (Right Outer Join)
+                      </a>
                     </li>
                     <li>
-                      <a href="#full-join">Full Join</a>
+                      <a className="sub-sub-link" href="#full-join">
+                        Full Join
+                      </a>
                     </li>
                     <li>
-                      <a href="#cross-join">Cross Join</a>
+                      <a className="sub-sub-link" href="#cross-join">
+                        Cross Join
+                      </a>
                     </li>
                   </ul>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#case-when">Case When</a>
-              <ul>
+              <a className="nav-link" href="#case-when">
+                Case When
+              </a>
+              <ul className="sub-list">
                 <li>
-                  <a href="#o-que-e-case">
+                  <a className="sub-link" href="#o-que-e-case">
                     O que é e como funciona o Case When
                   </a>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#criacao-instancia">Criação de Instância Via Terminal</a>
+              <a className="nav-link" href="#criacao-instancia">
+                Criação de Instância Via Terminal
+              </a>
             </li>
             <li>
-              <a href="#referencias">Referências</a>
+              <a className="nav-link" href="#referencias">
+                Referências
+              </a>
             </li>
           </ul>
         </nav>
-
-        <h2 id="paginas-extras">Páginas Extras</h2>
-        <ul>
-          <li>
-            <a href="https://www.notion.so/Cl-usulas-70c22f743c064af891f4fc8acc814c95?pvs=21">
-              Cláusulas
-            </a>
-          </li>
-          <li>
-            <a href="https://www.notion.so/Links-de-SQL-MySQL-cbe284bd02704c28a63c6a67563e226c?pvs=21">
-              Links de SQL/MySQL
-            </a>
-          </li>
-          <li>
-            <a href="https://www.notion.so/Connect-to-Database-SQL-Server-with-Java-a694730081914bd59b31a261d386eff2?pvs=21">
-              Connect to Database SQL Server with Java
-            </a>
-          </li>
-        </ul>
 
         <hr class="hr hr-blurry" />
         <section>
@@ -757,89 +811,97 @@ const SQLPage = () => {
             com cada linha da segunda tabela, resultando em um número de linhas
             igual ao produto dos números de linhas nas tabelas.
           </p>
+          <hr class="hr hr-blurry" />
         </section>
 
-        <h2 id="case-when">Case When</h2>
-
-        <h4 id="o-que-e-case">O que é e como funciona o Case When?</h4>
-        <p>
-          O <code>CASE WHEN</code> no SQL é uma função condicional que permite
-          definir regras específicas para operações em uma tabela de dados.
-          Assim, usamos quando uma condição específica é verdadeira. O{" "}
-          <code>CASE WHEN</code> está composto por três partes: a cláusula{" "}
-          <code>CASE</code>, a cláusula <code>WHEN</code> e a cláusula{" "}
-          <code>THEN</code>.
-        </p>
-        <pre>
-          <code>
-            {`CASE condição
-                WHEN valores_condicionais
-                THEN ações
-                ELSE ação_padrão
-                END;`}
-          </code>
-        </pre>
-        <p>Exemplo:</p>
-
-        <div className="code-container">
-          <button className="copy-button" onClick={copyToClipboard}>
-            Copy
-          </button>
-          <pre className="code-block" ref={codeRef}>
+        <section>
+          <h2 id="case-when">Case When</h2>
+          <h4 id="o-que-e-case">O que é e como funciona o Case When?</h4>
+          <p>
+            O <code>CASE WHEN</code> no SQL é uma função condicional que permite
+            definir regras específicas para operações em uma tabela de dados.
+            Assim, usamos quando uma condição específica é verdadeira. O{" "}
+            <code>CASE WHEN</code> está composto por três partes: a cláusula{" "}
+            <code>CASE</code>, a cláusula <code>WHEN</code> e a cláusula{" "}
+            <code>THEN</code>.
+          </p>
+          <pre>
             <code>
-              CASE WHEN condição1 THEN valor1 WHEN condição2 THEN valor2 ELSE
-              valorPadrão END;
+              {`CASE condição
+WHEN valores_condicionais
+THEN ações
+ELSE ação_padrão
+END;`}
             </code>
           </pre>
-          {showNotification && (
-            <div className="notification">
-              Código copiado para a área de transferência!
-            </div>
-          )}
-        </div>
-        <pre>
-          <code>{``}</code>
-        </pre>
+          <p>Exemplo:</p>
 
-        <h2 id="criacao-instancia">Criação de Instância Via Terminal</h2>
+          <div className="code-container">
+            <button className="copy-button" onClick={copyToClipboard}>
+              Copy
+            </button>
+            <pre className="code-block" ref={codeRef}>
+              <code>
+                CASE WHEN condição1 THEN valor1 WHEN condição2 THEN valor2 ELSE
+                valorPadrão END;
+              </code>
+            </pre>
+            {showNotification && (
+              <div className="notification">
+                Código copiado para a área de transferência!
+              </div>
+            )}
+          </div>
+          <pre>
+            <code>{``}</code>
+          </pre>
+          <hr class="hr hr-blurry" />
+        </section>
+        <section>
+          <h2 id="criacao-instancia">Criação de Instância Via Terminal</h2>
 
-        <h4>Pare e exclua a instância existente</h4>
-        <pre>
-          <code>
-            {`sqllocaldb stop "MinhaInstanciaAntiga"
+          <h4>Pare e exclua a instância existente</h4>
+          <pre>
+            <code>
+              {`sqllocaldb stop "MinhaInstanciaAntiga"
 sqllocaldb delete "MinhaInstanciaAntiga";`}
-          </code>
-        </pre>
+            </code>
+          </pre>
 
-        <h4>Crie uma nova instância</h4>
-        <pre>
-          <code>{`sqllocaldb create "MinhaNovaInstancia";`}</code>
-        </pre>
+          <h4>Crie uma nova instância</h4>
+          <pre>
+            <code>{`sqllocaldb create "MinhaNovaInstancia";`}</code>
+          </pre>
 
-        <h4>Inicie a nova instância</h4>
-        <pre>
-          <code>{`sqllocaldb start "MinhaNovaInstancia";`}</code>
-        </pre>
+          <h4>Inicie a nova instância</h4>
+          <pre>
+            <code>{`sqllocaldb start "MinhaNovaInstancia";`}</code>
+          </pre>
 
-        <h4>Verifique as informações da nova instância</h4>
-        <pre>
-          <code>{`sqllocaldb info "MinhaNovaInstancia";`}</code>
-        </pre>
+          <h4>Verifique as informações da nova instância</h4>
+          <pre>
+            <code>{`sqllocaldb info "MinhaNovaInstancia";`}</code>
+          </pre>
+        </section>
 
-        <h2>Referências</h2>
-        <p>
-          <a href="https://www.w3schools.com/sql/sql_intro.asp">
-            W3Schools SQL Tutorial
-          </a>
-        </p>
-        <p>
-          <a href="https://www.sqlservertutorial.net/">SQL Server Tutorial</a>
-        </p>
-        <p>
-          <a href="https://dev.mysql.com/doc/refman/8.0/en/">
-            MySQL Documentation
-          </a>
-        </p>
+        <section>
+          <hr class="hr hr-blurry" />
+
+          <h2 id="referencias">Referências</h2>
+          <p>
+            <a href="https://www.w3schools.com/sql/sql_intro.asp">
+              W3Schools SQL Tutorial
+            </a>
+          </p>
+          <p>
+            <a href="https://www.sqlservertutorial.net/">SQL Server Tutorial</a>
+          </p>
+          <p>
+            <a href="https://dev.mysql.com/doc/refman/8.0/en/">
+              MySQL Documentation
+            </a>
+          </p>
+        </section>
       </div>
     </div>
   );
