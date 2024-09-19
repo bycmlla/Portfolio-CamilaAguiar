48fea7377725eaf85d873c3bd8b84417b9e0d14c
Camila Carvalho <110317945+bycmlla@users.noreply.github.com>
2024-09-19 16:32:42 -0300

    fixes

--- src/pages/Projects/SubPages/Dashboards/Dashboards.js
+++ src/pages/Projects/SubPages/Dashboards/Dashboards.js
@@ -1,15 +1,38 @@
 import React from "react";
 import "./Dashboards.css";
+import DashboardVendas from "../../../../assets/images/dashboard 1.jpeg";
+import DashboardClientes from "../../../../assets/images/dashboard 3.png";
+import DashboardJornada from "../../../../assets/images/dashboard 5.png";
+import DashboardSquidGame from "../../../../assets/images/squidgame.png";
+import DashboardAlertas from "../../../../assets/images/alertas.png";
+import DashboardJornada2 from "../../../../assets/images/jornada.png";
 
 export const Dashboards = () => {
+  const images = [
+    { id: 1, path: DashboardVendas, alt: "Dashboard de Vendas" },
+    {
+      id: 2,
+      path: DashboardClientes,
+      alt: "Dashboard de Acompanhamento de Clientes",
+    },
+    { id: 3, path: DashboardJornada, alt: "Dashboard de Jornada de Motorista" },
+    { id: 4, path: DashboardSquidGame, alt: "Dashboard de Round 6" },
+    { id: 5, path: DashboardAlertas, alt: "Dashboard de Alertas" },
+    { id: 6, path: DashboardJornada2, alt: "Dashboard de Jornada" },
+  ];
   return (
     <div>
-
-        <div>
-            <img src=""/>
-        </div>
-
+      <div className="container-dashboards">
+        {images.map((image) => (
+          <div key={image.id} className="card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={image.path} alt={image.alt} />
+          </div>
+        ))}
+      </div>
       <div>
+
+        
+        {/* 
         <iframe
           title="aula-curso"
           width="825"
@@ -51,7 +74,7 @@ export const Dashboards = () => {
           height="500"
           src="https://app.powerbi.com/view?r=eyJrIjoiN2VmODQwNjktZjQwMi00NmQ0LTg2MzctMjc2MWEyOThlYTU2IiwidCI6ImU4YTlhNjA0LWIyYzItNDgzNy1hMDhkLTI1ZGRmNTBlMGU3OCJ9&pageName=b4d1351eb133b2a59ddb"
           allowFullScreen="true"
-        ></iframe>
+        ></iframe> */}
       </div>
     </div>
   );
