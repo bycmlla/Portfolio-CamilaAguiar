dcbe9d895e4723170c704680f13fa0fdf862fd8b
Camila Carvalho <110317945+bycmlla@users.noreply.github.com>
2024-09-19 11:59:57 -0300

    dashboard page insert

--- src/pages/Projects/SubPages/Dashboards/Dashboards.js
+++ src/pages/Projects/SubPages/Dashboards/Dashboards.js
@@ -1,9 +1,58 @@
 import React from "react";
+import "./Dashboards.css";
 
 export const Dashboards = () => {
-    return (
+  return (
+    <div>
+
         <div>
-            olá
+            <img src=""/>
         </div>
-    )
-}
+
+      <div>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiNDg4NWU3ZjgtYjdiOS00MThiLTlkMTktZjllNWU2NGZhYzQwIiwidCI6IjJhYTE4ODc1LTgwMjktNDkzZi1iMWEzLTE1M2FlNzllMjc4MCJ9"
+          allowFullScreen="true"
+        ></iframe>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiY2M2MGU4ZWUtNzYwOC00YWFjLTliM2EtZjFkYjcwNjM0NWQ4IiwidCI6IjJhYTE4ODc1LTgwMjktNDkzZi1iMWEzLTE1M2FlNzllMjc4MCJ9"
+          allowFullScreen="true"
+        ></iframe>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiNDU1MWE0YzYtNzJjYi00MjJmLWI1OTQtZDQ2ZWE3Y2RmMjY5IiwidCI6IjJhYTE4ODc1LTgwMjktNDkzZi1iMWEzLTE1M2FlNzllMjc4MCJ9"
+          allowFullScreen="true"
+        ></iframe>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiNjA5ZDRhMGEtOTBlZC00MTIwLWEzYmEtZDA5NjE0MDdjODA1IiwidCI6IjJhYTE4ODc1LTgwMjktNDkzZi1iMWEzLTE1M2FlNzllMjc4MCJ9"
+          allowFullScreen="true"
+        ></iframe>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiYzI1MTZjNDYtNzFjMS00ODdhLTgwMGEtYTIzNDVjMGU5NDUwIiwidCI6ImU4YTlhNjA0LWIyYzItNDgzNy1hMDhkLTI1ZGRmNTBlMGU3OCJ9"
+          allowFullScreen="true"
+        ></iframe>
+        <iframe
+          title="aula-curso"
+          width="825"
+          height="500"
+          src="https://app.powerbi.com/view?r=eyJrIjoiN2VmODQwNjktZjQwMi00NmQ0LTg2MzctMjc2MWEyOThlYTU2IiwidCI6ImU4YTlhNjA0LWIyYzItNDgzNy1hMDhkLTI1ZGRmNTBlMGU3OCJ9&pageName=b4d1351eb133b2a59ddb"
+          allowFullScreen="true"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
