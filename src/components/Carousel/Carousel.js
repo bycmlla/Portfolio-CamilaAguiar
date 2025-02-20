42fb41a17b8279f809de7d2a5d350ac592e5d01a
Camila Carvalho <110317945+bycmlla@users.noreply.github.com>
2025-02-20 11:06:15 -0300

    alterações no carrossel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./Carousel.css";
-import { motion } from "framer-motion";
+import { motion, useMotionValue, animate } from "framer-motion";
 import Dashboard from "../../assets/images/projects/carousel images/dashboard.png";
 import Pay2Me from "../../assets/images/projects/carousel images/pay2me.png";
 import Orion from "../../assets/images/projects/carousel images/orion.png";
 import RoomRover from "../../assets/images/projects/carousel images/RoomRover.png";
 import WeatherUp from "../../assets/images/projects/carousel images/weatherup.png";
 import Agrovet from "../../assets/images/projects/carousel images/agrovet.png";
+import "./Carousel.css";
 
 const images = [
   { src: Dashboard, text: "Dashboards Power BI" },
@@ -16,44 +16,58 @@ const images = [
   { src: WeatherUp, text: "Weather Up" },
   { src: Agrovet, text: "Cardoso Agrovet" },
 ];
-const infiniteImages = [...images, ...images, ...images, ...images, ...images];
+const infiniteImages = [...images, ...images, ...images];
 
 const Carousel = () => {
   const carousel = useRef();
-  const [width, setWidth] = useState(0);
-  const [position, setPosition] = useState(0);
-  const numImagesPerSlide = 3;
+  const x = useMotionValue(0);
+  const imageWidth = 450;
+  const visibleImages = 3;
   const totalImages = infiniteImages.length;
+  const loopLimit = imageWidth * images.length;
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
-  }, []);
+    let intervalId;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPosition((prev) => {
-        const newPosition = prev - width / (totalImages / numImagesPerSlide);
-        return newPosition <= -width ? 0 : newPosition;
-      });
-    }, 3000);
+    const startAutoScroll = () => {
+      intervalId = setInterval(() => {
+        animate(x, x.get() - imageWidth, {
+          duration: 1,
+          ease: "linear",
+          onComplete: () => {
+            // Lógica para o loop infinito
+            if (x.get() <= -loopLimit) {
+              x.set(0); // Reseta a posição para o início
+            }
+          },
+        });
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+    };
 
-    return () => clearInterval(interval);
-  }, [width]);
+    const stopAutoScroll = () => clearInterval(intervalId);
+
+    startAutoScroll();
+
+    return () => stopAutoScroll();
+  }, [x, imageWidth, loopLimit]); // Dependências importantes
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+    animate(x, -index * imageWidth, { duration: 0.5, ease: "easeOut" });
+  };
 
   return (
     <div className="carousel-body">
       <motion.div
         ref={carousel}
         className="carousel"
-        whileTap={{ cursor: "grabbing" }}
+        style={{ x }}
+        drag="x"
+        dragConstraints={{ left: -loopLimit, right: 0 }}
       >
-        <motion.div
-          className="inner"
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          initial={{ x: 0 }}
-          animate={{ x: position }}
-        >
+        <motion.div className="inner" style={{ display: "flex" }}>
           {infiniteImages.map((img, index) => (
             <motion.div className="item" key={index}>
               <img src={img.src} alt={`carousel-img-${index}`} />
@@ -63,6 +77,16 @@ const Carousel = () => {
           ))}
         </motion.div>
       </motion.div>
+
+      <div className="indicators">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`indicator ${currentIndex === index ? "active" : ""}`}
+            onClick={() => handleIndicatorClick(index)}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
